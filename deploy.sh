6abb041467321556e9de58447158100cb32c8276
Adam Kariv <adam.kariv@gmail.com>
2021-01-05 23:36:17 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export DOCKER_IMAGE=budgetkey/budgetkey-app-data-input
 (cd ui && npm run package) && \
+docker pull akariv/dgp-app && \
 docker build . -t $DOCKER_IMAGE && \
 docker push $DOCKER_IMAGE
